0ed24658669ff2ed5ed837b6e519a85457972d1e
KHong <albtrossfacefall@gmail.com>
2020-08-17 15:52:31 -0400

    test and implement router, first pass

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,12 @@
 // 2. components
 
 import React from 'react';
-
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import Layout from "./viewsCommon/Layout";
+import Home from "./views/Home";
+import About from "./views/About";
+
 
 
 // COMPONENTS & LOGIC
@@ -17,11 +20,18 @@ import Layout from "./viewsCommon/Layout";
 
 function App() {
   return (
-    <Layout>
-      <div>
-        i'm in
-      </div>
-    </Layout>
+    <BrowserRouter>
+      <Layout>
+        <div>
+          i'm in
+        </div>
+        <Routes>
+          <Route path="/about" element={<About />} />
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
   );
 }
 
